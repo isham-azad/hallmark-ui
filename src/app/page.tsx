e324d9251d4cf1390bff417bd9c8328c12f6d576
Isham Azad <ishamazad@gmail.com>
2026-07-04 13:35:23 +0400

    refactor: implement server-side product fetching and client-side cyclic pagination in HomeClient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
   const [websiteContent, products, brands, categories, testimonials] = await Promise.all([
     getSiteWebsiteContent(),
-    getSiteProducts(10),
+    getSiteProducts(),
     getSiteBrands(),
     getSiteCategories(),
     getSiteTestimonials(),
